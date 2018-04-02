a42024504fe5e08fe28d223f5969e6b2ebe4f881
Kévio Castro <keviocastro@gmail.com>
2018-04-02 17:21:40 -0300

    Removed mock server

--- scripts/mock-server.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash 
-
-source .env 
-./node_modules/json-server/bin/index.js \
--H ${MOCK_API_HOST} \
--c ./mock-server/json-server.json \
--s ./mock-server/public ./mock-server/db.json \
---middlewares ./mock-server/uploadImageMiddleware.js
